0259c6b09bac517db1810e7e492784287e7d3e6f
dagguapti vinaykumar <112471376+vinaykumardag1@users.noreply.github.com>
2024-08-08 11:03:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,9 +11,35 @@ app.use(cors({
     origin: "*"
 }));
 
-mongoose.connect("mongodb+srv://vinaykumardag1:vinayKOT@105@atlascluster.8tdja.mongodb.net/?retryWrites=true&w=majority&login_form=AtlasCluster", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("mongodb is connected"))
-    .catch(err => console.log("Database connection error:", err));
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://vinaykumardag1:vinayKOT@105@atlascluster.8tdja.mongodb.net/?retryWrites=true&w=majority&login_form=AtlasCluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.post('/register', (req, res) => {
     NewModel.create(req.body)
